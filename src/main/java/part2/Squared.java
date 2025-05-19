eff4b99f1593b9e717302d1709f69f04a7b0a12f
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2025-05-19 04:49:06 +0400

    part 2 code attached.

---
+++ src/main/java/part2/Squared.java
@@ -0,0 +1,15 @@
+package part2;
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        System.out.println(input * input);
+
+    }
+}
