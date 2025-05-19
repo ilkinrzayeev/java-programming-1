53f5029a1ed519452ac7a2db8025b27116d3eeab
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2025-05-19 04:50:00 +0400

    part 2 code attached.

---
+++ src/main/java/part2/SquareRootOfSum.java
@@ -0,0 +1,18 @@
+package part2;
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int in1 = scanner.nextInt();
+        int in2 = scanner.nextInt();
+
+        double sqr = Math.sqrt(in1 + in2);
+
+        System.out.println(sqr);
+
+    }
+}
