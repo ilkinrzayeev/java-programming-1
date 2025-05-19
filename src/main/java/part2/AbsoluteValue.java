eaa0fd1c3d702088979a0cef29e16fbdb3563512
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2025-05-19 04:50:54 +0400

    part 2 code attached.

---
+++ src/main/java/part2/AbsoluteValue.java
@@ -0,0 +1,18 @@
+package part2;
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int in = scanner.nextInt();
+
+        if(in < 0){
+            System.out.println(in *= -1);
+        } else {
+            System.out.println(in);
+        }
+    }
+}
