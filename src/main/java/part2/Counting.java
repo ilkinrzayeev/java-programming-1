0d576dedcbd963a49c0895827e2269f8437984d7
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2025-05-25 21:55:39 +0400

    part 2 code attached.

---
+++ src/main/java/part2/Counting.java
@@ -0,0 +1,16 @@
+package part2;
+
+import java.util.Scanner;
+
+public class Counting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int count = 0;
+
+        for(int i = 0; i <= input; i++) {
+            System.out.println(i);
+        }
+    }
+}
