cf92a0ff386636252720609b9ad779f013c6358c
Ilkin Rzayev <ilkinrzayev07700@gmail.com>
2025-05-25 21:57:03 +0400

    part 2 code attached.

---
+++ src/main/java/part2/CountingToHundred.java
@@ -0,0 +1,15 @@
+package part2;
+
+import java.util.Scanner;
+
+public class CountingToHundred {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        for(int c = input; c <= 100; c++) {
+            System.out.println(c);
+        }
+    }
+}
